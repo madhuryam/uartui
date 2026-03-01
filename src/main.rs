a858e5d01b5992a192b41c66a345f459151ba350
Madhurya Mahajan <madhuryamahajan@gmail.com>
2026-03-01 15:11:03 -0500

    making a main tui window, updating versions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("uartui");
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
 }
